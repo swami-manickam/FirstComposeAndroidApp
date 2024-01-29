35fbfb4c57403fb45c3e88dfe7786eb3c8c03eca
SwamiManickavasagam <swami.manickavasagam@disys.com>
2024-01-29 17:18:23 +0530

    Navigation bottom bar added

---
+++ app/src/main/java/com/mycompose/android/presentation/navigation/BottomNav.kt
@@ -0,0 +1,89 @@
+package com.mycompose.android.presentation.navigation
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.mycompose.android.presentation.screens.HomeScreen
+import com.mycompose.android.presentation.screens.NotificationScreen
+import com.mycompose.android.presentation.screens.ProfileScreen
+import com.mycompose.app.R
+
+
+@RequiresApi(Build.VERSION_CODES.M)
+@Composable
+fun BottomNav(navController: NavController) {
+
+    val items = listOf(
+        BottomNavItem.Home,
+        BottomNavItem.Profile,
+        BottomNavItem.Notification
+    )
+
+    BottomNavigation(
+        backgroundColor = colorResource(id = R.color.teal_200),
+        contentColor = Color.Black
+    ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+        items.forEach { item ->
+
+            //
+            BottomNavigationItem(
+                icon = { Icon(painterResource(id = item.icon), contentDescription = item.title) },
+                label = { Text(text = item.title,
+                    fontSize = 9.sp) },
+                selectedContentColor = Color.Black,
+                unselectedContentColor = Color.Black.copy(0.4f),
+                alwaysShowLabel = true,
+                selected = currentRoute == item.route,
+                onClick = {
+                    navController.navigate(item.route) {
+
+                        navController.graph.startDestinationRoute?.let { screen_route ->
+                            popUpTo(screen_route) {
+                                saveState = true
+                            }
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
+
+        }
+    }
+
+
+
+@Composable
+fun NavigationGraph(navController: NavHostController) {
+    NavHost(navController, startDestination = BottomNavItem.Home.route) {
+        composable(BottomNavItem.Home.route) {
+            HomeScreen()
+        }
+        composable(BottomNavItem.Profile.route) {
+            ProfileScreen()
+        }
+        composable(BottomNavItem.Notification.route) {
+            NotificationScreen()
+        }
+
+    }
+}
+
+
